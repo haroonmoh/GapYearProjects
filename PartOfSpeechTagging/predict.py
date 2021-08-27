84b89e2e0977da81a4d3c794a0d2b3f231c226cc
haroonmoh <60568923+haroonmoh@users.noreply.github.com>
2021-08-26 23:51:58 -0500

    Updated predict.py

--- PartOfSpeechTagging/predict.py
+++ PartOfSpeechTagging/predict.py
@@ -7,10 +7,16 @@ tag_counts = defaultdict(int, {'--SIN_UNK--': 4274862, '--VERB--': 1135394, '--U
 states = sorted(tag_counts.keys())
 vocab = {}
 
+A = np.loadtxt("/model/transition_matrix.txt.gz").reshape(14, 14)
+B = np.loadtxt("/model/emission_matrix.txt.gz").reshape(14, 183991)
+
 with gzip.open('/vocab/vocab.txt.gz','rt') as f:
     for line in f:
         word, number = line.split()
         vocab[word] = number
+
+#insert where user enters sentence. ^ should be loaded at all times. ↓ should run when user enters in sentence.
+sentence = ""
 
 def initialize(states, tag_counts, A, B, sentence, vocab):
     num_tags = len(tag_counts)
@@ -33,4 +39,67 @@ def initialize(states, tag_counts, A, B, sentence, vocab):
                         
     return best_probs, best_paths
 
+def viterbi_forward(A, B, test_corpus, best_probs, best_paths, vocab):
+
+    num_tags = best_probs.shape[0]
+    
+    for i in range(1, len(test_corpus)): 
+            
+        for j in range(num_tags):
+            
+            best_prob_i = float('-inf')
+            
+            best_path_i = None
+
+            for k in range(num_tags): 
+            
+                prob = best_probs[k, i-1] + math.log(A[k, j]) + math.log(B[j, vocab[test_corpus[i]]])
+
+                if prob > best_prob_i: 
+                    
+                    best_prob_i = prob
+
+                    best_path_i = k
+
+            best_probs[j,i] = best_prob_i
+            
+            best_paths[j,i] = best_path_i
+
+    return best_probs, best_paths
+
+def viterbi_backward(best_probs, best_paths, states):
+
+    m = best_paths.shape[1] 
+    
+    z = [None] * m
+    
+    num_tags = best_probs.shape[0]
+    
+    best_prob_for_last_word = float('-inf')
+    
+    pred = [None] * m
+    
+
+    for k in range(num_tags):
+
+        if best_probs[k, -1] > best_prob_for_last_word: 
+            
+            best_prob_for_last_word = best_probs[k, -1]
+    
+            z[m - 1] = k
+            
+    pred[m - 1] = states[k]
+    
+    for i in range(m-1, 0, -1): 
+        
+        pos_tag_for_word_i = best_paths[z[i], i]
+        
+        z[i - 1] = pos_tag_for_word_i
+        
+        pred[i - 1] = states[pos_tag_for_word_i] 
+        
+    return pred
+
 best_probs, best_paths = initialize(states, tag_counts, A, B, sentence, vocab)
+best_probs, best_paths = viterbi_forward(A, B, sentence, best_probs, best_paths, vocab)
+prediction = viterbi_backward(best_probs, best_paths, states)
